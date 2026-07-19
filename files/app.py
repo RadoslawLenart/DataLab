56933fe4492c537f3fb415896298a89c2bf62949
RadoslawLenart <radoslawlenart@onet.pl>
2026-07-19 15:59:07 +0200

    git commit -m "Add basic CSV data analysis"

--- files/app.py
+++ files/app.py
@@ -1,5 +1,8 @@
+from itertools import count
+
 from flask import Flask, render_template, request
 from pathlib import Path
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -17,6 +20,40 @@ def upload():
 
     file_path = UPLOAD_FOLDER/file.filename
     file.save(file_path)
+
+    df = pd.read_csv(file_path, sep='[;,]', engine='python')
+
+    info = {
+        'ilość_wierszy': df.shape[0],
+        'ilość_kolumn': df.shape[1],
+        'kolumny': list(df.columns)
+    }
+
+    missing = df.isnull().sum().to_dict()
+
+    print(f'Nazwa pliku:\n{file.filename}')
+    print('Podstawowe informacje:')
+    print(f'Ilość wierszy: {info['ilość_wierszy']}')
+    print(f'ilość_kolumn: {info['ilość_kolumn']}')
+    print('Kolumny:')
+    for column in info['kolumny']:
+        print(column)
+    print()
+    print('Brakujące dane:')
+    for column in missing.items():
+        print(f'{column[0]}: {column[1]}')
+    print()
+    print('Status:')
+    counter = 0
+    for column in missing.values():
+        if column == 0:
+            counter += 1
+            if counter == info['ilość_wierszy'] + 1:
+                print('Poprawny')
+        else:
+            print('Niepoprawny')
+            break
+
 
     return render_template('dashboard.html')
 
