1fb700303320f037c9f9ab83370a34af1cafcd80
RadoslawLenart <radoslawlenart@onet.pl>
2026-07-25 16:03:02 +0200

    refactor: improve dashboard styling and translate statistics

--- files/services/pdf_generator.py
+++ files/services/pdf_generator.py
@@ -1,16 +1,31 @@
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Image, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.pdfbase.ttfonts import TTFont, pdfmetrics
+from pathlib import Path
 
 
 def pdf_generator(analyst, charts, describe):
+    BASE_DIR = Path(__file__).parent
+
+    font_path = BASE_DIR / "fonts" / "DejaVuSans.ttf"
+
+    pdfmetrics.registerFont(
+        TTFont("DejaVu", str(font_path))
+    )
 
     styles = getSampleStyleSheet()
+
+    for style in styles.byName.values():
+        style.fontName = "DejaVu"
+    print(styles["Normal"].fontName)
+    print(styles["Heading1"].fontName)
+
 
     doc = SimpleDocTemplate('static/reports/raport.pdf')
 
     elements = []
 
-    elements.append(Paragraph("<b>Raport analizy danych</b>", styles["Heading1"]))
+    elements.append(Paragraph("Raport analizy danych", styles["Heading1"]))
     elements.append(Paragraph(f"Nazwa pliku: {analyst['Nazwa_pliku']}", styles["Normal"]))
     elements.append(Paragraph(f"Liczba wierszy: {analyst['ilość_wierszy']}", styles["Normal"]))
     elements.append(Paragraph(f"Liczba kolumn: {analyst['ilość_kolumn']}", styles["Normal"]))
@@ -18,7 +33,7 @@ def pdf_generator(analyst, charts, describe):
     elements.append(Spacer(1, 15))
 
     for statistic, columns in describe.items():
-        elements.append(Paragraph(statistic))
+        elements.append(Paragraph(statistic, styles["Normal"]))
         elements.append(Spacer(1,10))
         for column, value in columns.items():
             elements.append(Paragraph(column, styles["Normal"]))
--- files/services/statistics.py
+++ files/services/statistics.py
@@ -1,13 +1,13 @@
 def statistics(df):
     df = df.describe()
     describe = {
-        'count': df.iloc[0].to_dict(),
-        'mean': df.iloc[1].to_dict(),
-        'std': df.iloc[2].to_dict(),
-        'min': df.iloc[3].to_dict(),
-        '25%': df.iloc[4].to_dict(),
-        '50%': df.iloc[5].to_dict(),
-        '75%': df.iloc[6].to_dict(),
-        'max': df.iloc[7].to_dict(),
+        'Liczba wartości': df.iloc[0].to_dict(),
+        'Średnia': df.iloc[1].to_dict(),
+        'Odchylenie standardowe': df.iloc[2].to_dict(),
+        'Minimum': df.iloc[3].to_dict(),
+        'Pierwszy kwartyl (25%)': df.iloc[4].to_dict(),
+        'Mediana (50%)': df.iloc[5].to_dict(),
+        'Trzeci kwartyl (75%)': df.iloc[6].to_dict(),
+        'Maksimum': df.iloc[7].to_dict(),
     }
     return describe
